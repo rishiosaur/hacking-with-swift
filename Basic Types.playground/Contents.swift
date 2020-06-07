9e8287ab707bd65c1729bcd27e074d9bd69c9e38
Rishi Kothari <itsrishikothari@gmail.com>
2020-06-07 10:02:57 -0400

    add booleans

--- Basic Types.playground/Contents.swift
+++ Basic Types.playground/Contents.swift
@@ -11,3 +11,14 @@ let str1 = "Hello"
 let str2 = "World"
 
 let interpolatedString = "\(str1)\(str2)"
+
+
+// Booleans
+let bool1 = false
+let bool2 = true
+
+bool1 && bool2 // Returns false
+
+bool1 || bool2 // Returns true
+
+
